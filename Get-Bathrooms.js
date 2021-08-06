65f6f3f1d8ac6fefbedeaece97174114d94a9fb4
David Rouleau <shabbypenguin@gmail.com>
2021-08-06 16:31:39 -0400

    Start a good baseline

--- Get-Bathrooms.js
+++ Get-Bathrooms.js
@@ -1,51 +1,29 @@
-function getUserLocation(locationOutput){
-  fetch('https://ipapi.co/json')
+let map;
+
+
+function initMap(locationData) {
+  try{
+    console.log(locationData.latitude)
+    console.log(locationData.longitude)
+    map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: locationData.latitude, lng: locationData.longitude },
+    zoom: 14,
+      });
+  }
+  catch{}
+}
+
+async function getUserLocation(locationOutput){
+  await fetch('https://ipapi.co/json/')
   .then(response => {
     return response.json()
   }).then(result =>{
     locationOutput(result)
+    let map;
   })
 }
 
-let map;
-let service;
-let infowindow;
+getUserLocation(async (locationData)=> {
+  await initMap(locationData)
 
-function initMap(locationData) {
-  console.log(locationData.latitude)
-  const sydney = new google.maps.LatLng(locationData.latitude, locationData.longitude);
-  infowindow = new google.maps.InfoWindow();
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: sydney,
-    zoom: 15,
-  });
-  const request = {
-    query: "Museum of Contemporary Art Australia",
-    fields: ["name", "geometry"],
-  };
-  service = new google.maps.places.PlacesService(map);
-  service.findPlaceFromQuery(request, (results, status) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-      for (let i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-      map.setCenter(results[0].geometry.location);
-    }
-  });
-}
-
-function createMarker(place) {
-  if (!place.geometry || !place.geometry.location) return;
-  const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location,
-  });
-  google.maps.event.addListener(marker, "click", () => {
-    infowindow.setContent(place.name || "");
-    infowindow.open(map);
-  });
-}
-
-getUserLocation((locationData)=> {
-  initMap(locationData)
 })
