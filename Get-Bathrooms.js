5d7010d49b627e37718f356e14fba1c9bee12e1c
David Rouleau <shabbypenguin@gmail.com>
2021-08-10 12:32:58 -0400

    display info in the infowindows as well as get place id requests working

--- Get-Bathrooms.js
+++ Get-Bathrooms.js
@@ -1,9 +1,10 @@
 let map;
+let prev_infowindow =false;
 
 
 function initMap(locationData) {
   const mapCenter = new google.maps.LatLng(locationData.latitude,locationData.longitude)
-  infowindow = new google.maps.InfoWindow();
+  infowindow = new google.maps.InfoWindow()
   map = new google.maps.Map(document.getElementById("map"), {
       center: mapCenter,
       zoom: 12,
@@ -19,26 +20,61 @@ function initMap(locationData) {
       console.log(results)
       if (status === google.maps.places.PlacesServiceStatus.OK && results) {
         for (let i = 0; i < results.length; i++) {
+          //div.innerhtml = results[i]
           createMarker(results[i]);
         }
-        map.setCenter(results[0].geometry.location);
+        map.setCenter(results[0].geometry.location)
       }
     });
   }
   
-  function createMarker(place) {
+async function createMarker(place) {
+
     if (!place.geometry || !place.geometry.location) return;
     const marker = new google.maps.Marker({
       map,
       position: place.geometry.location,
+      title: place.name
     });
-    google.maps.event.addListener(marker, "click", () => { 
-      infowindow.setContent(place.name || ""); 
-      infowindow.open(map); 
-      console.log(place.place_id)
-      console.log(place.rating)
-    });
+    marker.addListener("click", () => {
+      if( prev_infowindow ) {
+        prev_infowindow.close()
+      }
+      
+      getPlaceInformation(place.place_id, function(place) {
+        if (place.rating == null){
+          newrating = "not rated"
+        }else {
+          newrating = `rated ${place.rating}`
+        }
+        
+        console.log(place)
+        let contentInfo = `<h4>${place.name} is ${newrating} you can reach them at ${place.formatted_phone_number}<h4>`
+        
+        const infowindow = new google.maps.InfoWindow({
+          content: contentInfo
+        })
+        prev_infowindow = infowindow;
+        infowindow.open({
+          anchor: marker,
+          map,
+          shouldFocus: false,
+        })
+      })
+    })
   }
+
+function getPlaceInformation(placeid, completion) {
+  var request = {
+    placeId: placeid,
+    fields: ['name', 'rating', 'formatted_phone_number', 'geometry', 'review']
+  };
+  
+  service = new google.maps.places.PlacesService(map)
+  service.getDetails(request, function(place, status) {
+    completion(place)
+  })
+}
 
 async function getUserLocation(locationOutput){
   await fetch('https://ipapi.co/json/')
@@ -46,7 +82,7 @@ async function getUserLocation(locationOutput){
     return response.json()
   }).then(result =>{
     locationOutput(result)
-    let map;
+    let map
   })
 }
 
